5ba5c1eac723f2e29199e6875061cb816822a519
e200 <eleandro@inbox.ru>
2017-09-04 14:01:10 +0100

    Now increment and decrement uses getLastIndex()

--- src/SliderPlex.js
+++ src/SliderPlex.js
@@ -248,7 +248,7 @@
 
         /** Increment the current slide position */
         function increment() {
-            if($index === $slides_length - 1)
+            if($index === getLastIndex())
                 $index = 0;
             else
                 $index++;
@@ -257,7 +257,7 @@
         /** Decrement the current slide position */
         function decrement() {
             if($index === 0)
-                $index = $slides_length - 1;
+                $index = getLastIndex();
             else
                 $index--;
         }
@@ -268,162 +268,3 @@
         return $plex;
     }
 })(jQuery);
-
-/*
-$(function () {
-
-
-    var sliderContents = $('.slider ul li');
-    var cycle;
-
-    function initSlider() {
-        setZIndexes();
-
-        startNewCycle(8, slideRight);
-    }
-
-    function setZIndexes() {
-
-        sliderContents
-            .first()
-            .addClass($active)
-            .next()
-            .addClass($next);
-
-        sliderContents
-            .last()
-            .addClass('prev');
-    }
-
-    initSlider();
-
-    $('.arrow-left').on('click', function () {
-        slideLeft();
-    });
-    $('.arrow-right').on('click', function () {
-        slideRight();
-        startNewCycle(8, slideRight);
-    });
-
-    function startNewCycle(time, callback) {
-        if(cycle)
-            clearInterval(cycle);
-
-        cycle = setInterval(callback, time * 1000);
-    }
-
-    function slideRight() {
-        slideOutOldSlide();
-        setTimeout(function () {
-            removeClassesFromOldSlide();
-            activeNextSlide();
-            setNextSlide();
-            setPrevSlide()
-        }, 500);
-    }
-    function slideLeft() {
-        slideOutOldSlide();
-
-        setTimeout(function () {
-            removeClassesFromOldSlide();
-            activePrevSlide();
-            setNextSlide();
-            setPrevSlide()
-        }, 500);
-    }
-
-    /!**
-     * Retorna o slide apresentado actualmente
-     *
-     * @returns {*|jQuery|HTMLElement}
-     *!/
-    function activeSlide() {
-        return $('.slider .active');
-    }
-
-    /!**
-     * Retorna o slide a anteriro do corrente
-     *
-     * @returns {*|jQuery|HTMLElement}
-     *!/
-    function nextSlide() {
-        return $('.slider .next');
-    }
-
-    /!**
-     * Retorna o slide a seguir do corrente
-     *
-     * @returns {*|jQuery|HTMLElement}
-     *!/
-    function prevSlide() {
-        return $('.slider .prev');
-    }
-
-    /!**
-     * Activa a animação de retirada do slide anterior
-     *!/
-    function slideOutOldSlide() {
-        activeSlide().addClass('out');
-    }
-
-    /!**
-     * Remove as classes `.active .out` do slide anterior
-     *!/
-    function removeClassesFromOldSlide() {
-        activeSlide().removeClass('active out');
-    }
-
-    /!**
-     * Activa (apresenta) o próximo slide
-     *!/
-    function activeNextSlide() {
-        nextSlide()
-            .removeClass($next)
-            .addClass($active);
-    }
-
-    /!**
-     * Activa (apresenta) o slide anterior
-     *!/
-    function activePrevSlide() {
-        prevSlide()
-            .removeClass('prev')
-            .addClass($active);
-    }
-
-    /!**
-     * Define quem será o slide a ser apresentado a seguir
-     *!/
-    function setNextSlide() {
-        var nextSlide = activeSlide().next();
-
-        /!* Se haver um slide a seguir ao `.active`,
-         * defina-o como `.next` se não, defina
-         * o primeiro slide.
-         *!/
-        if(nextSlide.length)
-            nextSlide.addClass($next);
-        else
-            sliderContents
-                .first()
-                .addClass($next);
-    }
-
-    /!**
-     * Define quem será o slide a ser apresentado a seguir
-     *!/
-    function setPrevSlide() {
-        var prevSlide = activeSlide().prev();
-
-        /!* Se haver um slide a seguir ao `.active`,
-         * defina-o como `.next` se não, defina
-         * o primeiro slide.
-         *!/
-        if(prevSlide.length)
-            prevSlide.addClass('prev');
-        else
-            sliderContents
-                .last()
-                .addClass('prev');
-    }
-});*/
